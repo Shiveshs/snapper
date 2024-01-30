570f03d1d33805561f6a37e9c4f9af712c53a323
Shivesh Shandilya <shivesh.shandilya@gmail.com>
2024-01-31 01:11:17 +0530

    Added Tailwind CSS

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,27 @@ import "./globals.css";
 import { Routes, Route } from "react-router-dom";
 import SigninForm from "./_auth/forms/SigninForm";
 import SignupForm from "./_auth/forms/SignupForm";
-import {Home} from "./_root/pages";
+import AuthLayout from "./_auth/AuthLayout";
+import RootLayout from "./_root/RootLayout";
+import { Home } from "./_root/pages";
 
 const App = () => {
   return (
     <main className="flex h-screen">
       <Routes>
         {/* Public Routes */}
-        <Route path="/sign-in" element={<SigninForm/>}/>
-        <Route path="/sign-up" element={<SignupForm/>}/>
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SigninForm />} />
+          <Route path="/sign-up" element={<SignupForm />} />
+        </Route>
 
         {/* Private Routes */}
-        <Route index element={<Home />}/>
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
       </Routes>
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import { Outlet, Navigate } from "react-router-dom";
 
 const AuthLayout = () => {
-  return (
-    <div>AuthLayout</div>
-  )
-}
+  const isAuthenticated: boolean = false;
 
-export default AuthLayout
+  return (
+    <>
+      {isAuthenticated ? (
+        <Navigate to='/' />
+      ) : (
+        <>
+          <section className="flex flex-1 justify-center items-center flex-col py-10">
+            <Outlet/>
+          </section>
+          <img src="/assets/images/side-img.svg"
+          alt="logo"
+          ></img>
+        </>
+      )}
+    </>
+  );
+};
+
+export default AuthLayout;
