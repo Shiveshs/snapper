0c2dd0ecd4589e3e3bcffe6c05a2c014cf8da84c
Shivesh Shandilya <shivesh.shandilya@gmail.com>
2024-01-30 23:27:53 +0530

    Added Routing for the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import "./globals.css"
+import "./globals.css";
+import { Routes, Route } from "react-router-dom";
+import SigninForm from "./_auth/forms/SigninForm";
+import SignupForm from "./_auth/forms/SignupForm";
+import {Home} from "./_root/pages";
 
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-        Hello Snapper!
-    </h1>
+    <main className="flex h-screen">
+      <Routes>
+        {/* Public Routes */}
+        <Route path="/sign-in" element={<SigninForm/>}/>
+        <Route path="/sign-up" element={<SignupForm/>}/>
+
+        {/* Private Routes */}
+        <Route index element={<Home />}/>
+      </Routes>
+    </main>
   )
 }
 
