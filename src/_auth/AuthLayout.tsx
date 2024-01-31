2ed5a390af72796b472842b4e23363618a6ae60a
Shivesh Shandilya <shivesh.shandilya@gmail.com>
2024-01-31 16:37:13 +0530

    Adding form to sign up page

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -14,7 +14,7 @@ const AuthLayout = () => {
           </section>
           <img src="/assets/images/side-img.svg"
           alt="logo"
-          ></img>
+          className="hidden xl:block h-screen w-1/2 object-cover bg-no-repeat"></img>
         </>
       )}
     </>
