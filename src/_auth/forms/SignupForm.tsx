77716fd6cf975fedc66e4d45d4eee6a550739e8f
Shivesh Shandilya <shivesh.shandilya@gmail.com>
2024-01-31 23:03:06 +0530

    Complete Sign-up Page

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -13,29 +13,55 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-
-const formSchema = z.object({
-  username: z.string().min(2).max(50),
-});
+import { signupValidation } from "@/lib/validation";
+import Loader from "@/components/shared/Loader";
+import { Link } from "react-router-dom";
 
 const SignupForm = () => {
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+  const isLoading = false;
+  const form = useForm<z.infer<typeof signupValidation>>({
+    resolver: zodResolver(signupValidation),
     defaultValues: {
+      name: "",
       username: "",
+      email: "",
+      password: "",
     },
   });
 
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values);
+  async function onSubmit(values: z.infer<typeof signupValidation>) {
+    // const newUser = await createUserAccount(values)
   }
   return (
-    <div>
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-8'>
+    <Form {...form}>
+      <div className='sm:w-420 flex-center flex-col'>
+        <div className="flex">
+        <img className="col-span-2 max-h-12 w-full object-contain lg:col-span-1 rotate-6" src='/assets/images/SNAPPER.svg' /><h2 className="h4-bold md:h3-bold md:pt-4 sm:pt-4 pl-2">napper</h2>
+        </div>
+        
+        <h2 className='h3-bold md:h2-bold pt-2 sm:pt-2'>
+          Create a new account
+        </h2>
+        <p className='text-light-3 small-medium md:base-regular mt-2'>
+          To use Snapper, please enter your details
+        </p>
+        <form
+          className='flex flex-col gap-5 w-full mt-4'
+          onSubmit={form.handleSubmit(onSubmit)}>
+          <FormField
+            control={form.control}
+            name='name'
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Name</FormLabel>
+                <FormControl>
+                  <Input type='text' className='shad-input' {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
           <FormField
             control={form.control}
             name='username'
@@ -43,19 +69,61 @@ const SignupForm = () => {
               <FormItem>
                 <FormLabel>Username</FormLabel>
                 <FormControl>
-                  <Input placeholder='shadcn' {...field} />
+                  <Input type='text' className='shad-input' {...field} />
                 </FormControl>
-                <FormDescription>
-                  This is your public display name.
-                </FormDescription>
                 <FormMessage />
               </FormItem>
             )}
           />
-          <Button type='submit'>Submit</Button>
+          <FormField
+            control={form.control}
+            name='email'
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input type='email' className='shad-input' {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name='password'
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input type='password' className='shad-input' {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type='submit' className='shad-button_primary'>
+            {isLoading ? (
+              <div className='flex-center gap-2'>
+                <Loader />
+                Loading...
+              </div>
+            ) : (
+              "Sign Up"
+            )}
+          </Button>
+          <p
+            className='text-small-regular 
+          text-light-2 text-center mt-2'>
+            Already have an account?{" "}
+            <Link
+              to='/sign-in'
+              className='text-primary-500 text-small-semibold ml-1'>
+              Log in
+            </Link>
+          </p>
         </form>
-      </Form>
-    </div>
+      </div>
+    </Form>
   );
 };
 
